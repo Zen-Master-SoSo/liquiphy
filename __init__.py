2ddfc3eb7805ff47e7d9950bf7644ff367466d5b
Ripley Believe It <liyang@holly.home>
2024-12-01 21:55:53 +0800

    Read help for list of commands

--- __init__.py
+++ __init__.py
@@ -2,32 +2,58 @@
 #
 #  Copyright 2024 liyang <liyang@veronica>
 #
-import subprocess, io, os, logging
+"""
+Interface with LiquidSFZ. To see availeble commnds:
+
+	with LiquidSFZ(filename) as liquid:
+		print(liquid.help())
+
+"""
+import subprocess, io, os, re, logging
 from time import sleep
 from signal import signal, SIGINT, SIGTERM
 
+PROMPT = 'liquidsfz> '
+HELP_REGEX = '^(\w+)\s([^\-]+)\-\s(.*)'
 
 class LiquidSFZ:
 
 	def __init__(self):
 		signal(SIGINT, self.system_signal)
 		signal(SIGTERM, self.system_signal)
+		self.stay_alive = True
 		self.process = subprocess.Popen(
-			["liquidsfz", "/mnt/data-drive/docs/sfz/Drumsets/Pearl-Masters/Pearl.sfz"],
+			["liquidsfz", "/home/liyang/docs/sfz/Drumsets/Pearl-Masters/Pearl.sfz"],
 			encoding="ASCII",
 			stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-		self.stay_alive = True
 		self.read_response()
-		self.write("help\n")
-		self.read_response()
-		self.write("quit\n")
+		self.write('help')
+		helptext = self.read_response()
+		commands = {
+			tup[0]:tup[1:] \
+			for tup in [
+				self._interpret_helptext(line) \
+				for line in helptext.split("\n")
+				if re.match(HELP_REGEX, line)
+			]
+		}
+		self.write('quit')
 		while self.process.poll() is None:
 			logging.debug('waiting')
-			sleep(.5)
+			sleep(.2)
 
-	def write(self, stuff):
-		logging.debug('Writing "%s"' % stuff.rstrip())
-		self.process.stdin.write(stuff)
+	def _interpret_helptext(self, line):
+		m = re.match(HELP_REGEX, line)
+		args = m[2].strip()
+		return (
+			m[1],
+			args.split(' ') if args else [],
+			m[3]
+		)
+
+	def write(self, command):
+		logging.debug('Writing "%s"' % command)
+		self.process.stdin.write(command + os.linesep)
 		self.process.stdin.flush()
 
 	def read_response(self):
@@ -36,20 +62,29 @@ class LiquidSFZ:
 		line = ''
 		while self.stay_alive:
 			char = self.process.stdout.read(1)
-			buf.write(char)
 			if char == os.linesep:
+				buf.write(line)
+				buf.write(char)
 				line = ''
 			else:
-				line = line + char
-			if line == 'liquidsfz> ':
-				logging.debug('Got "liquidsfz> " prompt - break')
+				line += char
+			if line == PROMPT:
+				logging.debug('Got "%s" prompt - break', PROMPT)
 				break
-		logging.debug('Done reading')
 		buf.seek(0)
-		print(buf.read())
+		return buf.read()
 
-	def system_signal(self, sig, frame):
+	def __enter__(self):
+		return self
+
+	def __exit__(self, *_):
+		self._terminate()
+
+	def system_signal(self, *_):
 		logging.debug("Caught signal - shutting down")
+		self._terminate()
+
+	def _terminate(self):
 		self.process.terminate()
 		self.stay_alive = False
 
@@ -58,6 +93,7 @@ log_level = logging.DEBUG
 log_format = "[%(filename)24s:%(lineno)4d] %(levelname)-8s %(message)s"
 logging.basicConfig(level = log_level, format = log_format)
 
-LiquidSFZ()
+with LiquidSFZ() as liquid:
+	print(liquid.help())
 
 #  end liquiphy/__init__.py
