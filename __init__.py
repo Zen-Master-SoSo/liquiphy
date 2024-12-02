93aa81dbd4c4d61078f255f14c97af54f5d13cf7
Ripley Believe It <liyang@holly.home>
2024-12-02 08:12:52 +0800

    Basically works

--- __init__.py
+++ __init__.py
@@ -10,37 +10,34 @@ Interface with LiquidSFZ. To see availeble commnds:
 
 """
 import subprocess, io, os, re, logging
+from functools import partial
 from time import sleep
 from signal import signal, SIGINT, SIGTERM
 
-PROMPT = 'liquidsfz> '
-HELP_REGEX = '^(\w+)\s([^\-]+)\-\s(.*)'
+PROMPT		= 'liquidsfz> '
+HELP_REGEX	= '^(\w+)\s([^\-]+)\-\s(.*)'
+USAGE_ERR	= 'Usage: LiquidSFZ.%s(%s)'
 
 class LiquidSFZ:
 
 	def __init__(self):
-		signal(SIGINT, self.system_signal)
-		signal(SIGTERM, self.system_signal)
+		signal(SIGINT, self._system_signal)
+		signal(SIGTERM, self._system_signal)
 		self.stay_alive = True
 		self.process = subprocess.Popen(
 			["liquidsfz", "/home/liyang/docs/sfz/Drumsets/Pearl-Masters/Pearl.sfz"],
 			encoding="ASCII",
 			stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-		self.read_response()
+		logging.debug(self.read_response())
 		self.write('help')
 		helptext = self.read_response()
-		commands = {
-			tup[0]:tup[1:] \
-			for tup in [
-				self._interpret_helptext(line) \
-				for line in helptext.split("\n")
-				if re.match(HELP_REGEX, line)
-			]
-		}
-		self.write('quit')
-		while self.process.poll() is None:
-			logging.debug('waiting')
-			sleep(.2)
+		logging.debug('Setting functions')
+		for tup in [
+			self._interpret_helptext(line) \
+			for line in helptext.split("\n")
+			if re.match(HELP_REGEX, line)
+		]:
+			setattr(self, tup[0], partial(self._exec, tup))
 
 	def _interpret_helptext(self, line):
 		m = re.match(HELP_REGEX, line)
@@ -51,6 +48,12 @@ class LiquidSFZ:
 			m[3]
 		)
 
+	def _exec(self, funcsig, /, *args):
+		if len(funcsig[1]) != len(args):
+			raise UsageError(USAGE_ERR % (funcsig[0], ", ".join(funcsig[1])))
+		self.write(funcsig[0] + " " + " ".join(args))
+		return self.read_response()
+
 	def write(self, command):
 		logging.debug('Writing "%s"' % command)
 		self.process.stdin.write(command + os.linesep)
@@ -59,13 +62,13 @@ class LiquidSFZ:
 	def read_response(self):
 		logging.debug('Reading response')
 		buf = io.StringIO()
-		line = ''
+		line = str()
 		while self.stay_alive:
 			char = self.process.stdout.read(1)
 			if char == os.linesep:
 				buf.write(line)
 				buf.write(char)
-				line = ''
+				line = str()
 			else:
 				line += char
 			if line == PROMPT:
@@ -80,7 +83,7 @@ class LiquidSFZ:
 	def __exit__(self, *_):
 		self._terminate()
 
-	def system_signal(self, *_):
+	def _system_signal(self, *_):
 		logging.debug("Caught signal - shutting down")
 		self._terminate()
 
@@ -88,12 +91,31 @@ class LiquidSFZ:
 		self.process.terminate()
 		self.stay_alive = False
 
+class UsageError(Exception):
+	pass
 
-log_level = logging.DEBUG
-log_format = "[%(filename)24s:%(lineno)4d] %(levelname)-8s %(message)s"
-logging.basicConfig(level = log_level, format = log_format)
+def log_error(e):
+	tb = e.__traceback__
+	logging.error('%s %s(), line %s: %s "%s"',
+		os.path.basename(tb.tb_frame.f_code.co_filename),
+		tb.tb_frame.f_code.co_name,
+		tb.tb_lineno,
+		type(e).__name__,
+		str(e),
+	)
 
-with LiquidSFZ() as liquid:
-	print(liquid.help())
+if __name__ == "__main__":
+	log_level = logging.DEBUG
+	log_format = "[%(filename)24s:%(lineno)4d] %(levelname)-8s %(message)s"
+	logging.basicConfig(level = log_level, format = log_format)
+
+	with LiquidSFZ() as liquid:
+		print(dir(liquid))
+		try:
+			liquid.help('bad arg', 'another')
+		except UsageError as e:
+			log_error(e)
+		print(liquid.help())
+		sleep(.25)
 
 #  end liquiphy/__init__.py
