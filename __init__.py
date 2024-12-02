4f4ef293462e71e48cd9b8f7bc637980b34da031
Ripley Believe It <liyang@holly.home>
2024-12-03 05:05:10 +0800

    Add LiquidJack class

--- __init__.py
+++ __init__.py
@@ -13,11 +13,14 @@ Interface with LiquidSFZ. To see availeble commnds:
 import subprocess, io, os, re, logging
 from functools import partial
 from signal import signal, SIGINT, SIGTERM
+from jack import Client
 from good_logging import log_error
 
 PROMPT		= 'liquidsfz> '
 HELP_REGEX	= '^(\w+)\s([^\-]+)\-\s(.*)'
 USAGE_ERR	= 'Usage: LiquidSFZ.%s(%s) # %s'
+JACK_NAME	= 'liquidjack'
+
 
 class LiquidSFZ:
 
@@ -91,6 +94,33 @@ class LiquidSFZ:
 
 class UsageError(Exception):
 	pass
+
+class LiquidJack(LiquidSFZ):
+	"""
+	LiquidSFZ client with added Jack audio tools functionality.
+	"""
+
+	midi_in = None
+	audio_out_1 = None
+	audio_out_2 = None
+
+	def __init__(self, filename = None):
+		self.client = Client(JACK_NAME, no_start_server=True)
+		self.client.set_port_registration_callback(self.port_registration_callback)
+		self.client.activate()
+		super().__init__(filename)
+
+	def port_registration_callback(self, port, register):
+		"""
+		Called by Jack when port is added or removed
+		"""
+		if register and 'liquidsfz' in port.name:
+			if port.is_midi and self.midi_in is None:
+				self.midi_in = port
+			elif 'audio_out_1' in port.name and self.audio_out_1 is None:
+				self.audio_out_1 = port
+			elif 'audio_out_2' in port.name and self.audio_out_2 is None:
+				self.audio_out_2 = port
 
 
 if __name__ == "__main__":
