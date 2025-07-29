762b629c22feae43a159efc37a03f615fe7c187a
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2025-07-29 15:14:17 +0800

    Prepare for publishing

--- liquiphy/__init__.py
+++ liquiphy/__init__.py
@@ -2,8 +2,25 @@
 #
 #  Copyright 2024 liyang <liyang@veronica>
 #
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
 """
-Interface with LiquidSFZ. To see available commnds:
+A quick and dirty interface to the liquidsfz command-line using python's subprocess.
+
+To see available commnds:
 
 	with LiquidSFZ(filename) as liquid:
 		print(dir(liquid))
@@ -14,6 +31,8 @@ import subprocess, io, os, re, logging
 from functools import partial
 from threading import Thread
 from queue import Queue, Empty
+
+__version__ = "1.0.0"
 
 PROMPT		= 'liquidsfz> '
 HELP_REGEX	= '^(\w+)\s([^\-]+)\-\s(.*)'
