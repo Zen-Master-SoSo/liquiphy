02c7f63edf7f1993f625e4d6d89604523cacdfc0
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2026-08-02 12:38:30 +0800

    Bump version 1.3.0 -> 1.3.1

--- liquiphy/__init__.py
+++ liquiphy/__init__.py
@@ -33,7 +33,7 @@ from functools import partial
 from threading import Thread
 from queue import Queue, Empty
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 PROMPT		= 'liquidsfz> '
 HELP_REGEX	= rcompile(r'^(\w+)\s([^\-]+)\-\s(.*)')
